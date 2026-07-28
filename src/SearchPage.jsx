e0eab0b9dc106f0c66eaf3be0a6f5c89bdac946f
minibgg <ivangey984@gmail.com>
2026-07-28 14:57:50 +0300

    hide dota, add info about me

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -32,59 +32,70 @@ export default function SearchPage() {
   };
 
   return (
-    <span>
-      <button onClick={goToDotaTracker}>dota 2</button>
-      <div className="searchPageMain">
-        <div className="searchStack">
-          <div className="maininput">
-            <select
-              className="regioninput"
-              onChange={(e) => setRegion(e.target.value)}
-              value={region}
-            >
-              <option value="EUW">EUW</option>
-              <option value="RU">RU</option>
-              <option value="NA">NA</option>
-              <option value="KR">KR</option>
-              <option value="BR">BR</option>
-              <option value="TR">TR</option>
-            </select>
-            <input
-              className="textinput"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="MishaCrazy#RU1"
-            />
-            <button className="searchbtn" onClick={handleSearch}>
-              Search
-            </button>
-          </div>
-          <div className="secondinput">
-            <select
-              className="regioninput"
-              onChange={(e) => setRegion(e.target.value)}
-              value={region}
-            >
-              <option value="EUW">EUW</option>
-              <option value="RU">RU</option>
-              <option value="NA">NA</option>
-              <option value="KR">KR</option>
-              <option value="BR">BR</option>
-              <option value="TR">TR</option>
-            </select>
-            <input
-              className="textinput"
-              value={secondInput}
-              onChange={(e) => setSecondInput(e.target.value)}
-              placeholder="Aura Farmium#RUNit"
-            />
-            <button className="searchbtn" onClick={Compare}>
-              Compare
-            </button>
-          </div>
+    <>
+      <div className="infoBorder">
+        <div>if u have some idea dm me:</div>
+        <div>
+          discord: <strong>louise_francoise_de_la_valliere</strong>
+        </div>
+        <div>
+          telegram: <strong>@MiniBggtg</strong>
         </div>
       </div>
-    </span>
+      <span>
+        {/* <button onClick={goToDotaTracker}>dota 2</button> */}
+        <div className="searchPageMain">
+          <div className="searchStack">
+            <div className="maininput">
+              <select
+                className="regioninput"
+                onChange={(e) => setRegion(e.target.value)}
+                value={region}
+              >
+                <option value="EUW">EUW</option>
+                <option value="RU">RU</option>
+                <option value="NA">NA</option>
+                <option value="KR">KR</option>
+                <option value="BR">BR</option>
+                <option value="TR">TR</option>
+              </select>
+              <input
+                className="textinput"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="MishaCrazy#RU1"
+              />
+              <button className="searchbtn" onClick={handleSearch}>
+                Search
+              </button>
+            </div>
+            <div className="secondinput">
+              <select
+                className="regioninput"
+                onChange={(e) => setRegion(e.target.value)}
+                value={region}
+              >
+                <option value="EUW">EUW</option>
+                <option value="RU">RU</option>
+                <option value="NA">NA</option>
+                <option value="KR">KR</option>
+                <option value="BR">BR</option>
+                <option value="TR">TR</option>
+              </select>
+              <input
+                className="textinput"
+                value={secondInput}
+                onChange={(e) => setSecondInput(e.target.value)}
+                placeholder="Aura Farmium#RUNit"
+              />
+              <button className="searchbtn" onClick={Compare}>
+                Compare
+              </button>
+            </div>
+          </div>
+        </div>
+      </span>
+    </>
   );
 }
 //test
