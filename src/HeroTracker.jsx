23d88461a1f7dba73b05fbaf02815460cdd18dac
minibgg <ivangey984@gmail.com>
2026-07-28 16:02:12 +0300

    remove import

--- src/HeroTracker.jsx
+++ src/HeroTracker.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { riotApi } from "./riotApi";
 
 export default function HeroTracker() {
   const [champions, setChampions] = useState(null);
