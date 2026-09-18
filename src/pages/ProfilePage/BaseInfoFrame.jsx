b88de6b264d0a43b0f78bad7c5c34592b4644c67
barringgton <ivangey984@gmail.com>
2026-09-18 22:58:47 +0300

    feat(BaseInfoFrame): add copie playername and tag button

--- src/pages/ProfilePage/BaseInfoFrame.jsx
+++ src/pages/ProfilePage/BaseInfoFrame.jsx
@@ -1,5 +1,6 @@
 import { SummonerIcon } from "./SummonerIcon.jsx";
 import { getRankColor, getWinStreak } from "../../service";
+import { useState } from "react";
 
 function RankRow({ label, queue }) {
   const color = getRankColor(queue?.tier);
@@ -17,6 +18,14 @@ function RankRow({ label, queue }) {
 }
 
 export function BaseInfoFrame({ data, soloQ, rankedflex, rankedPremade }) {
+  const [copied, setCopied] = useState(false);
+  const copy = () => {
+    navigator.clipboard.writeText(
+      `${data.account?.gameName}#${data.account?.tagLine}`,
+    );
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
   const soloQColor =
     soloQ?.tier && soloQ.tier.toUpperCase() !== "UNRANKED"
       ? getRankColor(soloQ.tier)
@@ -44,6 +53,10 @@ export function BaseInfoFrame({ data, soloQ, rankedflex, rankedPremade }) {
         )}
         <h2 style={{ margin: 0 }}>
           {data.account?.gameName}#{data.account?.tagLine}
+          <button onClick={copy} title="Copy">
+            {" "}
+            {copied ? "✓" : "📋"}{" "}
+          </button>
         </h2>
       </div>
 
